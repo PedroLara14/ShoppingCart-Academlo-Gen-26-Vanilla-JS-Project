3c6491af0a09b325aeae197e16faf6c268f21cbf
Pedro Lara <brownsgameplays14@gmail.com>
2023-05-02 04:29:26 -0500

    Second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,37 +27,20 @@ function printProducts(db) {
       </div>
 
       <div class="product__info">
-        <h3>${product.name} <span> <b>Stock</b>: ${product.quantity}</span> </h3>
-        <h4>
-          ${product.price}
-          <i class='bx bx-plus' id='${product.id}'></i>   
-        </h4>
+        <i class='bx bx-plus' id='${product.id}'></i>
+        <h3>
+          $${product.price}.00
+          <span class="product__info--stock">Stock: ${product.quantity}</span>
+        </h3>
+        <p class="showModalProduct">
+          ${product.name}
+        </p>
       </div>
     </div>
     `;
   }
 
   productsHTML.innerHTML = html;
-}
-
-function handleShowCart() {
-  const openCartIconHTML = document.querySelector(".bx-shopping-bag")
-  const cartHTML = document.querySelector(".contentCart");
-
-  openCartIconHTML.addEventListener("click", function () {
-    cartHTML.classList.add("contentCart_show");
-  });
-
-}
-
-function handleCloseCart() {
-  const closeIconCartHTML = document.querySelector(".bx-x");
-  const cartHTML = document.querySelector(".contentCart");
-
-  closeIconCartHTML.addEventListener("click", function () {
-    cartHTML.classList.remove("contentCart_show");
-  });
-
 }
 
 async function main() {
@@ -69,27 +52,6 @@ async function main() {
   };
 
   printProducts(db);
-  handleShowCart();
-  handleCloseCart(); 
-
-  const productsHTML = document.querySelector(".products");
-
-  productsHTML.addEventListener("click", function (e) {
-    if (e.target.classList.contains("bx-plus")) {
-      const id = Number(e.target.id);
-
-      const productFind = db.products.find(product => product.id === id);
-
-      if (db.cart[productFind.id]) {
-        if (productFind.quantity === db.cart[productFind.id].amount) {
-          return alert("No tenemos mas en bodega");
-        }  
-        db.cart[productFind.id].amount++;
-      } else {
-        db.cart[productFind.id] = {...productFind, amount: 1};
-      }
-    }
-  })
 }
 
 main();
